92466c163ce14e4d61ef692d6ea6d24a765a97c2
student <student@gccis-wicmac01.main.ad.rit.edu>
2018-12-15 17:26:59 -0500

    Added skeleton controllers for each view

---
+++ DnD-5e-Character-Sheet/DnD-5e-Character-Sheet/controllers/BasicStatsVC.swift
@@ -0,0 +1,35 @@
+//
+//  BasicStatsVC.swift
+//  DnD-5e-Character-Sheet
+//
+//  Created by student on 12/15/18.
+//  Copyright © 2018 Regina Locicero. All rights reserved.
+//
+
+import UIKit
+
+class BasicStatsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
