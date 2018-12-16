2c8b96c740ffc908740db331e454437fb3c7bddd
student <student@gccis-wicmac01.main.ad.rit.edu>
2018-12-16 18:09:06 -0500

    Got navigation for edit to work

---
+++ DnD-5e-Character-Sheet/DnD-5e-Character-Sheet/controllers/EditBasicStatsVC.swift
@@ -0,0 +1,38 @@
+//
+//  EditBasicStatsVC.swift
+//  DnD-5e-Character-Sheet
+//
+//  Created by student on 12/16/18.
+//  Copyright © 2018 Regina Locicero. All rights reserved.
+//
+
+import UIKit
+
+class EditBasicStatsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func doneEditing(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
