0440c86a4f5105ec3c51fb1500eb20b5c1b78b94
student <student@gccis-wicmac01.main.ad.rit.edu>
2018-12-16 19:33:06 -0500

    Added edit outlets

--- DnD-5e-Character-Sheet/DnD-5e-Character-Sheet/controllers/EditBasicStatsVC.swift
+++ DnD-5e-Character-Sheet/DnD-5e-Character-Sheet/controllers/EditBasicStatsVC.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class EditBasicStatsVC: UIViewController {
 
+    @IBOutlet weak var characterNameField: UITextField!
+    @IBOutlet weak var raceField: UITextField!
+    @IBOutlet weak var classField: UITextField!
+    @IBOutlet weak var lvlField: UITextField!
+    @IBOutlet weak var speedField: UITextField!
+    @IBOutlet weak var acField: UITextField!
+    @IBOutlet weak var maxHPField: UITextField!
+    @IBOutlet weak var currHPField: UITextField!
+    @IBOutlet weak var maxHDField: UITextField!
+    @IBOutlet weak var currHDField: UITextField!
+    @IBOutlet weak var hdtypeField: UITextField!
+    
+    @IBOutlet weak var strField: UITextField!
+    @IBOutlet weak var strProfSwitch: UISwitch!
+    
+    @IBOutlet weak var intField: UITextField!
+    @IBOutlet weak var intProfSwitch: UISwitch!
+    
+    @IBOutlet weak var conField: UITextField!
+    @IBOutlet weak var conProfSwitch: UISwitch!
+    
+    @IBOutlet weak var wisField: UITextField!
+    @IBOutlet weak var wisProfSwitch: UISwitch!
+    
+    @IBOutlet weak var dexField: UITextField!
+    @IBOutlet weak var dexProfSwitch: UISwitch!
+    
+    @IBOutlet weak var charField: UITextField!
+    @IBOutlet weak var charProfSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,9 +49,6 @@ class EditBasicStatsVC: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    @IBAction func doneEditing(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
     }
     
 
