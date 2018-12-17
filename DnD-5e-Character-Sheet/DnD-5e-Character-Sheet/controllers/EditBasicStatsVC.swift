3308c1c0351e9364a5fdb9387e80eed98451c7e8
student <student@gccis-wicmac01.main.ad.rit.edu>
2018-12-16 20:11:15 -0500

    Added filling in defaults

--- DnD-5e-Character-Sheet/DnD-5e-Character-Sheet/controllers/EditBasicStatsVC.swift
+++ DnD-5e-Character-Sheet/DnD-5e-Character-Sheet/controllers/EditBasicStatsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class EditBasicStatsVC: UIViewController {
 
@@ -43,7 +44,48 @@ class EditBasicStatsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let realm = try! Realm()
+        let characters = realm.objects(Character.self)
+        
+        if !characters.isEmpty {
+            // If it's not empty then there is already a character so fill in the fields with what is already displayed
+            let c = characters.first!
+            
+            characterNameField.text = c.characterName
+            raceField.text = c.race
+            classField.text = c.characterClass
+            lvlField.text = String(c.level)
+            speedField.text = String(c.speed)
+            acField.text = String(c.ac)
+            maxHPField.text = String(c.healthStats!.maxHP)
+            currHPField.text = String(c.healthStats!.currentHP)
+            maxHDField.text = String(c.healthStats!.hitDiceMax)
+            currHDField.text = String(c.healthStats!.hitDiceCurr)
+            hdtypeField.text = String(c.healthStats!.hitDiceType)
+            
+            strField.text = String(c.coreStats!.strength)
+            intField.text = String(c.coreStats!.intelligence)
+            conField.text = String(c.coreStats!.constitution)
+            wisField.text = String(c.coreStats!.wisdom)
+            dexField.text = String(c.coreStats!.dexterity)
+            charField.text = String(c.coreStats!.charisma)
+            
+            strProfSwitch.isOn = isProficient(prof: c.coreStats!.strengthProf)
+            intProfSwitch.isOn = isProficient(prof: c.coreStats!.intelligenceProf)
+            conProfSwitch.isOn = isProficient(prof: c.coreStats!.constitutionProf)
+            wisProfSwitch.isOn = isProficient(prof: c.coreStats!.wisdomProf)
+            dexProfSwitch.isOn = isProficient(prof: c.coreStats!.dexterityProf)
+            charProfSwitch.isOn = isProficient(prof: c.coreStats!.charismaProf)
+        }
+    }
+    
+    private func isProficient(prof: Int) -> Bool {
+        if prof == 1 {
+            return true
+        }
+        else {
+            return false
+        }
     }
 
     override func didReceiveMemoryWarning() {
